996c1a77331b5840af4645a3d93b8302e1e8070e
Roman Muratov <velikbmx69@yandex.ru>
2020-06-23 18:18:54 +0300

    add bootstrap

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -3,10 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
@@ -22,9 +19,10 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
-    public String printUsers(ModelMap model) {
+    public String printUsers(@SessionAttribute("user") User user, ModelMap model) {
         List<User> list = userService.listUsers();
         model.addAttribute("list", list);
+        model.addAttribute("currentUser", user);
         return "users";
     }
 
