866aca77b225dc2a2c114a82ffd29ec5c34a601c
Roman Muratov <velikbmx69@yandex.ru>
2020-06-30 23:04:47 +0300

    add bootstrap

--- src/main/java/web/controller/AdminRestController.java
+++ src/main/java/web/controller/AdminRestController.java
@@ -1,9 +1,8 @@
 package web.controller;
 
 import com.google.gson.Gson;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
 import web.service.UserService;
 
 @RestController
@@ -21,5 +20,17 @@ public class AdminRestController {
         Gson gson = new Gson();
         String json = gson.toJson(userService.listUsers());
         return json;
+    }
+
+    @PostMapping("admin/add")
+    public void addUser(@RequestBody String user) {
+        User userFromJson = getUser(user);
+        userService.save(userFromJson);
+    }
+
+    private User getUser(String user) {
+        Gson gson = new Gson();
+        User userFromJson = gson.fromJson(user, User.class);
+        return userFromJson;
     }
 }
--- src/main/resources/static/test2.js
+++ src/main/resources/static/test2.js
@@ -2,23 +2,69 @@ $(document).ready(function () {
     $.ajax({
         type: 'GET',
         url: '/rest',
-        dataType:"json",
+        dataType: "json",
         success: function (list) {
-            let $tbody = $("#tbody");
-            for(let i = 0; i < list.length; i++) {
-                $tbody.append("<tr>");
-                $tbody.append(`<td>${list[i].id}</td>`);
-                $tbody.append(`<td>${list[i].email}</td>`);
-                $tbody.append(`<td>${list[i].password}</td>`);
+            let tbody = $("#tbody");
+            for (let i = 0; i < list.length; i++) {
+                tbody.append(`<tr>`);
+                tbody.append(`<td>${list[i].id}</td>`);
+                tbody.append(`<td>${list[i].email}</td>`);
+                tbody.append(`<td>${list[i].password}</td>`);
                 let roleTd = `<td>`;
-                for(let role in list[i].roles) {
+                for (let role in list[i].roles) {
                     roleTd += list[i].roles[role].name;
                 }
-                $tbody.append(roleTd + "</td>");
-                $tbody.append(`<td><button type="button" class="btn btn-primary" data-toggle="modal">Edit</button></td>`)
-                $tbody.append(`<td><button type="button" class="btn btn-danger" data-toggle="modal">Delete</button></td>`)
-                $tbody.append("</tr>");
+                tbody.append(roleTd + "</td>");
+                tbody.append(`<td><button type="button" class="btn btn-primary" value="${list[i].id}" name="editModal" 
+                    data-toggle="modal">Edit</button></td>`);
+                tbody.append(`<td><button type="button" class="btn btn-danger" value="${list[i].id}" name="deleteModal" 
+                    data-toggle="modal">Delete</button></td>`);
+                tbody.append(`</tr>`);
             }
         }
     })
+});
+
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: '/rest/admin/delete',
+        dataType: "JSON",
+        success: function (user) {
+
+        }
+    })
 })
+
+function addUser() {
+    let dataFromForm = {
+        email: $("#addEmail").val(),
+        password: $("#addPassword").val(),
+        //roles: $("#addRoles").val()
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/rest/admin/add',
+        data: JSON.stringify(dataFromForm),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            $('#result').html(data);
+        }
+    })
+}
+
+/*function addUser() {
+    $.ajax("/rest/admin/add", "POST", {
+        data: {
+            email: $("#addEmail").val(),
+            password: $("#addPassword").val(),
+            roles: $("#addRoles").val()
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $(".result").html(data);
+        }
+    })
+}*/
